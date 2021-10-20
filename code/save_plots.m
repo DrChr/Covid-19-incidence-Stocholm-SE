f8efb07a1299c2fe0ae2ad92e8c89c4df1b284e8
Christian Ridderström <christian.ridderstrom@gmail.com>
2021-10-20 20:31:16 +0200

    Fix out dir to docs/

--- code/save_plots.m
+++ code/save_plots.m
@@ -1,5 +1,5 @@
 function save_plots()
-  save_as = @(fig, name) saveas(fig, fullfile('public', name));
+  save_as = @(fig, name) saveas(fig, fullfile('docs', name));
   save_as(1, 'Covid-19_incidence_Sweden_SE.png');
   save_as(2, 'Covid-19_incidence_Stockholm_SE.png');
   save_as(3, 'Covid-19_incidence_mean_SE.png');
